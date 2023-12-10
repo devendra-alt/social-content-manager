5d7b65d596927811a5e61d183d56d5abaf4177d5
devendra-alt <devendramulewa171@gmail.com>
2023-12-11 02:17:01 +0530

    Add graphql provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:social_content_manager/public/login.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  MyApp({Key? key}) : super(key:key) ;
+  MyApp({Key? key}) : super(key: key);
 
   final HttpLink httpLink = HttpLink('https://eksamaj.in/meelangraphql ');
 
   @override
   Widget build(BuildContext context) {
-
     final GraphQLClient client = GraphQLClient(
       link: httpLink,
       cache: GraphQLCache(),
     );
 
     return GraphQLProvider(
-      client: ValueNotifier(client),
-      child:MaterialApp(
-        title: '',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(title: 'Social Content Managment!'),
-      )
-    );
+        client: ValueNotifier(client),
+        child: MaterialApp(
+          title: '',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(title: 'Social Content Managment!'),
+        ));
   }
 }
 
