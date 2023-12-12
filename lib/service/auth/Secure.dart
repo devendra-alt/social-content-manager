816b440d863a6e13729a930c91b3d7ef8cef419e
devendra-alt <devendramulewa171@gmail.com>
2023-12-12 18:54:34 +0530

    Refactor Secure.dart

--- lib/service/auth/Secure.dart
+++ lib/service/auth/Secure.dart
@@ -2,14 +2,14 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 final storage = FlutterSecureStorage();
 
-void writeToSecureStorage(key,value) async{
+void writeToSecureStorage(key, value) async {
   await storage.write(key: key, value: value);
 }
 
-Future<String?> readFromSecureStorage(key) async{
+Future<String?> readFromSecureStorage(key) async {
   return await storage.read(key: key);
 }
 
-void deleteFromSecureStorage(key) async{
+void deleteFromSecureStorage(key) async {
   await storage.delete(key: key);
 }
