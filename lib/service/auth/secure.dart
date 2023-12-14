60c4f246effef4d1c85289fcefb799145d1d405f
devendra-alt <devendramulewa171@gmail.com>
2023-12-14 13:51:01 +0530

    Add return Type [Future<boolean>] for writeToSecureStorage method in Secure.dart

--- lib/service/auth/secure.dart
+++ lib/service/auth/secure.dart
@@ -2,6 +2,16 @@ Future<bool> writeToSecureStorage(key, value) async {
   try {
     await storage.write(key: key, value: value);
     return true;
+Future<bool> writeToSecureStorage(key, value) async {
+  try {
+    await storage.write(key: key, value: value);
+    return true;
   } catch (e) {
     return false;
+  }
+    await storage.write(key: key, value: value);
+    return true;
+  } catch (e) {
+    return false;
+  }
   }
