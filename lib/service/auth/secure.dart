07b960b97982ac8e0b614501ab76e6c1c25b0b0d
devendra-alt <devendramulewa171@gmail.com>
2023-12-14 13:53:07 +0530

    Add logout button routine for profile page

--- lib/service/auth/secure.dart
+++ lib/service/auth/secure.dart
@@ -1,17 +1,15 @@
-Future<bool> writeToSecureStorage(key, value) async {
-  try {
-    await storage.write(key: key, value: value);
-    return true;
-Future<bool> writeToSecureStorage(key, value) async {
-  try {
-    await storage.write(key: key, value: value);
-    return true;
-  } catch (e) {
-    return false;
-  }
-    await storage.write(key: key, value: value);
-    return true;
-  } catch (e) {
-    return false;
-  }
-  }
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final storage = FlutterSecureStorage();
+
+void writeToSecureStorage(key, value) async {
+  await storage.write(key: key, value: value);
+}
+
+Future<String?> readFromSecureStorage(key) async {
+  return await storage.read(key: key);
+}
+
+void deleteFromSecureStorage(key) async {
+  await storage.delete(key: key);
+}
