c80645142d6553a7625df99bd4348f4f8378e099
devendra-alt <devendramulewa171@gmail.com>
2023-12-16 17:01:06 +0530

    Add user StateNotifier

--- lib/service/auth/user.dart
+++ lib/service/auth/user.dart
@@ -1,8 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class User extends StateNotifier<User> {
+class User {
   String username;
   String email;
-  User({required this.username, required this.email})
-      : super(User(email: email, username: username));
+  int id;
+  User({required this.username, required this.email, required this.id});
+}
+
+class UserData extends StateNotifier<User> {
+  UserData()
+      : super(User(username: "John Doe", email: "JohnDoe@123.com", id: 1));
+  void addData(String email, String username, int id) {
+    state.email = email;
+    state.username = username;
+    state.id = id;
+  }
 }
